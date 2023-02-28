55e4480b4b6299876993907f9387f5009b9d5580
multityy <59746858+jip-erk@users.noreply.github.com>
2023-02-28 14:44:09 +0100

    🚀

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+const pressed = reactive(useMousePressed());
+const pages = [
+  { name: "index", route: "/" },
+  { name: "test", route: "/test" },
+];
+const currentPage = ref(1);
+const router = useRouter();
+watch(pressed, () => {
+  router.push({ path: pages[currentPage.value].route });
+});
+</script>
+
+<style>
+.page-left-enter-active,
+.page-right-enter-active,
+.page-left-leave-active,
+.page-right-leave-active {
+  transition: all 0.3s linear;
+}
+
+.page-left-enter-from,
+.page-right-leave-to {
+  transform: translateX(100%);
+}
+
+.page-left-leave-to,
+.page-right-enter-from {
+  transform: translateX(-100%);
+}
+
+.page-left-enter-to,
+.page-right-enter-to {
+  transform: translateX(0);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex h-screen">
+    <div class="m-auto">
+      <h1 class="text-6xl mb-4 font-bold">powerpoint</h1>
+      <h1 class="font-medium">Door: jip, matt, mees, floris</h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
